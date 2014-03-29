942b64f73af524985511401fbbbbd38c5eb8c872
Joe Lepper <jlepper@newrelic.com>
2014-03-28 23:23:43 -0700

    makes config object pattern work on all routes / preps manifest

--- chrome/new-issue.js
+++ chrome/new-issue.js
@@ -1,16 +1,11 @@
 (function() {
-
-  var client = new GitHubSelfies(
-    ["button:contains(Submit new issue)"],
-    "#issue_body",
-    "#totallyAwesomeSelfieButton",
-    "#selfieVideo",
-    "#selfieCanvas");
-
-  client.buttonHTML = '<button id="totallyAwesomeSelfieButton" type="button" class="button" onclick="return false;" style="margin-right: 5px;"><span class="octicon octicon-device-camera" style="font-size: 22px; margin-right: 5px; line-height: 0px;"></span>Selfie!</button>';
-  client.videoHTML = '<video autoplay id="selfieVideo" style="display: none;"></video>';
-  client.canvasHTML = '<canvas id="selfieCanvas" style="display: none;"></canvas>';
-
+  var config = {
+      insertBefore   : ["button:contains(Submit new issue)"],
+      bodySelector   : "#issue_body",
+      buttonHTML     : '<button id="totallyAwesomeSelfieButton" type="button" class="button" onclick="return false;" style="margin-right: 5px;"><span class="octicon octicon-device-camera" style="font-size: 22px; margin-right: 5px; line-height: 0px;"></span>Selfie!</button>',
+      x              : 300,
+      y              : 200,
+    }
+    , client = new GitHubSelfies(config);
   client.setupSelfieStream();
-
 })();
--- chrome/pull.js
+++ chrome/pull.js
@@ -7,12 +7,7 @@
         'button:contains(Comment)'
       ],
       bodySelector   : '[name="comment[body]"]',
-      buttonSelector : '#totallyAwesomeSelfieButton',
-      videoSelector  : '#selfieVideo',
-      canvasSelector : '#selfieCanvas',
       buttonHTML     : '<button id="totallyAwesomeSelfieButton" type="button" class="button" onclick="return false;" style="margin-right: 5px;"><span class="octicon octicon-device-camera" style="font-size: 22px; margin-right: 5px; line-height: 0px;"></span>Selfie!</button>',
-      videoHTML      : '<video autoplay id="selfieVideo" style="display: none;"></video>',
-      canvasHTML     : '<canvas id="selfieCanvas" style="display: none;"></canvas>',
       x              : 300,
       y              : 200
     }
@@ -30,9 +25,9 @@
 
   // github hides instead of deleting dom elements when you submit
   var cleanup = function() {
-    $(config.buttonSelector).remove();
-    $(config.videoSelector).remove();
-    $(config.canvasSelector).remove();
+    $('#totallyAwesomeSelfieButton').remove();
+    $('#selfieVideo').remove();
+    $('#selfieCanvas').remove();
   };
 
   $('[name="comment_and_open"]').on('click', cleanup);
