76e9132fc6f4ad3acb0e921ac438ea64f11e07e9
Joe Lepper <jlepper@newrelic.com>
2014-03-31 10:47:32 -0700

    gets video feedback working on all routes

--- chrome/pull.js
+++ chrome/pull.js
@@ -6,10 +6,14 @@
         '[name="comment_and_close"][type="submit"]',
         'button:contains(Comment)'
       ],
-      bodySelector   : '[name="comment[body]"]',
-      buttonHTML     : '<button id="totallyAwesomeSelfieButton" type="button" class="button" onclick="return false;" style="margin-right: 5px;"><span class="octicon octicon-device-camera" style="font-size: 22px; margin-right: 5px; line-height: 0px;"></span>Selfie!</button>',
-      x              : 300,
-      y              : 200
+      bodySelector : '[name="comment[body]"]',
+      buttonHTML   : '<button id="totallyAwesomeSelfieButton" type="button" class="button" onclick="return false;" style="margin-right: 5px;"><span class="octicon octicon-device-camera" style="font-size: 22px; margin-right: 5px; line-height: 0px;"></span>Selfie!</button>',
+      x            : 300,
+      y            : 200,
+      placeVideo   : function (video, canvas) {
+        $($('.discussion-sidebar-item')[$('.discussion-sidebar-item').length - 1]).prepend(video);
+        $($('.discussion-sidebar-item')[$('.discussion-sidebar-item').length - 1]).prepend(canvas);
+      }
     }
     , client = new GitHubSelfies(config);
 
