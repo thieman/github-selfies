fc222b905fc19d83148c6c94404487b73475e6e5
Joe Lepper <jlepper@newrelic.com>
2014-04-01 21:28:24 -0700

    performance tuning and UI tweaks

--- chrome/new-issue.js
+++ chrome/new-issue.js
@@ -1,14 +1,14 @@
 (function() {
   var config = {
-      insertBefore   : ["button:contains(Submit new issue)"],
-      bodySelector   : "#issue_body",
-      buttonHTML     : '<button id="totallyAwesomeSelfieButton" type="button" class="button" onclick="return false;" style="margin-right: 5px;"><span class="octicon octicon-device-camera" style="font-size: 22px; margin-right: 5px; line-height: 0px;"></span>Selfie!</button>',
-      x              : 300,
-      y              : 200,
-      placeVideo   : function (video, canvas) {
-        console.log('placing video!!!')
-        $('.form-actions').append(video);
-      },
+      insertBefore : ['button:contains(Submit new issue)'],
+      bodySelector : '#issue_body',
+      buttonHTML   : (
+        '<button id="totallyAwesomeSelfieButton" type="button" class="button">' +
+          '<span class="octicon octicon-device-camera"></span>' +
+          'Selfie!' +
+        '</button>'
+      ),
+      placeVideo    : function (video, canvas) { $('.form-actions').append(video); },
       placeCheckBox : function (checkbox, button) {
         $(checkbox).insertBefore(button);
         $('.selfieCheckBoxContainer').addClass('newIssueSelfieCheckBoxContainer');
