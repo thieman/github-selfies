d96e7e785bc3d9a41374a0f70a63e2197036d8cd
Ben Hollis <ben@benhollis.net>
2015-12-14 23:12:50 -0800

    Fixes to the styling and injection.

--- chrome/selfie-base.js
+++ chrome/selfie-base.js
@@ -1,7 +1,7 @@
 // Create a selfie button in the given form.
 function GitHubSelfieButtons() {
   this.elem = $(
-    '<div class="github-selfies github-selfies--selfieControls">' +
+    '<div class="github-selfies" id="selfieControls">' +
     '<button type="button" class="selfieToggle btn btn-default">Video instead</button>' +
     '<button type="button" class="totallyAwesomeSelfieButton btn btn-default">' +
     '<span class="octicon octicon-device-camera"></span>' +
@@ -27,67 +27,69 @@ function GitHubSelfieButtons() {
         this.onselfie(this.dynamic);
       }
     }.bind(this));
-  this.selfieToggle.on('click', this.toggleSelfieType);
+  this.selfieToggle.on('click', this.toggleSelfieType.bind(this));
 }
 
-GitHubSelfieButtons.prototype.insertBefore = function(element) {
-  // Be careful not to insert more than once!
-  this.elem.eq(0).insertBefore(element);
-  return this;
-};
+GitHubSelfieButtons.prototype = {
+  insert: function(element) {
+    // Be careful not to insert more than once!
+    this.elem.eq(0).insertAfter(element);
+    return this;
+  },
 
-GitHubSelfieButtons.prototype.hide = function() {
-  this.elem.addClass('github-selfies--hidden');
-  return this;
-};
-GitHubSelfieButtons.prototype.show = function() {
-  this.elem.removeClass('github-selfies--hidden');
-  return this;
-};
+  hide: function() {
+    this.elem.addClass('hidden');
+    return this;
+  },
+  show: function() {
+    this.elem.removeClass('hidden');
+    return this;
+  },
 
-GitHubSelfieButtons.prototype.destroy = function() {
-  this.elem.remove();
-  this.selfieButton = null;
-  this.selfieToggle = null;
-};
+  destroy: function() {
+    this.elem.remove();
+    this.selfieButton = null;
+    this.selfieToggle = null;
+  },
 
-GitHubSelfieButtons.prototype.showVideoPreview = function() {
-  if (this.videoPreview === null) {
-    // Insert the preview before the buttons
-    this.videoPreview = new GitHubSelfieVideoPreview();
-    this.videoPreview.insertBefore(this.elem);
+  showVideoPreview: function() {
+    if (this.videoPreview === null) {
+      // Insert the preview before the buttons
+      this.videoPreview = new GitHubSelfieVideoPreview();
+      this.videoPreview.insert(this.elem);
+    }
+    this.videoPreview.show();
+    return this;
+  },
+  disableButton: function() {
+    this.selfieButton.prop('disabled', true);
+    return this;
+  },
+  enableButton: function() {
+    this.selfieButton.prop('disabled', false);
+    return this;
+  },
+
+  hideVideoPreview: function() {
+    if (this.videoPreview !== null) {
+      this.videoPreview.hide();
+      // TODO: destroy??
+    }
+    return this;
+  },
+
+  toggleSelfieType: function() {
+    this.selfieToggle.text(this.dynamic ? 'Photo instead' : 'Video instead');
+    this.selfieButton.find('span')
+      .toggleClass('octicon-device-camera octicon-device-camera-video');
+    this.selfieToggle.toggleClass('selected dark-grey');
+    this.dynamic = !this.dynamic;
   }
-  this.videoPreview.show();
-  return this;
 };
-GitHubSelfieButtons.prototype.disableButton = function() {
-  this.selfieButton.prop('disabled', true);
-  return this;
-};
-GitHubSelfieButtons.prototype.enableButton = function() {
-  this.selfieButton.prop('disabled', false);
-  return this;
-};
-
-GitHubSelfieButtons.prototype.hideVideoPreview = function() {
-  if (this.videoPreview !== null) {
-    this.videoPreview.hide();
-    // TODO: destroy??
-  }
-  return this;
-};
-
-GitHubSelfieButtons.prototype.toggleSelfieType = function() {
-  this.selfieToggle.text(this.dynamic ? 'Photo instead' : 'Video instead');
-  this.selfieButton.toggleClass('octicon-device-camera octicon-device-camera-video');
-  this.selfieToggle.toggleClass('selected dark-grey');
-  this.dynamic = !this.dynamic;
-};
-
 
 function GitHubSelfieVideoPreview() {
   this.elem = $(
-    '<div class="github-selfies github-selfies--selfieVideoContainer github-selfies--hidden">' +
+    '<div class="github-selfies selfieVideoContainer hidden">' +
     '<div class="selfieProgressContainer"><div class="selfieProgress"></div></div>' +
     '<div class="selfie-countdown">' +
     '<div class="counter-container"></div>' +
@@ -105,115 +107,117 @@ function GitHubSelfieVideoPreview() {
   this.stream = null;
 }
 
-GitHubSelfieVideoPreview.prototype.insertBefore = function(element) {
-  // Be careful not to insert more than once!
-  this.elem.eq(0).insertBefore(element);
-  return this;
-};
+GitHubSelfieVideoPreview.prototype = {
+  insert: function(element) {
+    // Be careful not to insert more than once!
+    this.elem.eq(0).insertBefore(element);
+    return this;
+  },
 
-GitHubSelfieVideoPreview.prototype.resizeCanvas = function(isDynamic) {
-  this.canvasElem.setAttribute('height', this.videoElem.videoHeight / (isDynamic ? 3 : 1));
-  this.canvasElem.setAttribute('width', this.videoElem.videoWidth /  (isDynamic ? 3 : 1));
-  return this;
-};
+  resizeCanvas: function(isDynamic) {
+    this.canvasElem.setAttribute('height', this.videoElem.videoHeight / (isDynamic ? 3 : 1));
+    this.canvasElem.setAttribute('width', this.videoElem.videoWidth /  (isDynamic ? 3 : 1));
+    return this;
+  },
 
-GitHubSelfieVideoPreview.prototype.hide = function() {
-  this.elem.addClass('github-selfies--hidden');
-  return this;
-};
-GitHubSelfieVideoPreview.prototype.show = function() {
-  this.elem.removeClass('github-selfies--hidden');
-  return this;
-};
+  hide: function() {
+    this.elem.addClass('hidden');
+    return this;
+  },
+  show: function() {
+    this.elem.removeClass('hidden');
+    return this;
+  },
 
-GitHubSelfieVideoPreview.prototype.setMessage = function(message) {
-  this.textOverlay.text(message);
-  return this;
-};
+  setMessage: function(message) {
+    this.textOverlay.text(message);
+    return this;
+  },
 
-GitHubSelfieVideoPreview.prototype.stopPreview = function() {
-  // TODO: destroy??
-  this.stream.stop();
-  this.stream = null;
-};
+  stopPreview: function() {
+    // TODO: destroy??
+    this.stream.stop();
+    this.stream = null;
+  },
 
-GitHubSelfieVideoPreview.prototype.destroy = function() {
-  this.stopPreview();
-  this.elem.remove();
-  this.videoElem = null;
-  this.canvasElem = null;
-  this.textOverlay = null;
-  this.counterContainer = null;
-};
+  destroy: function() {
+    this.stopPreview();
+    this.elem.remove();
+    this.videoElem = null;
+    this.canvasElem = null;
+    this.textOverlay = null;
+    this.counterContainer = null;
+  },
 
-GitHubSelfieVideoPreview.prototype.startPreview = function(buttons) {
-  this.setMessage('Fetching camera stream...');
+  startPreview: function(buttons) {
+    this.setMessage('Fetching camera stream...');
 
-  // TODO: handle video stream events
-  // TODO: gotta clean up previous stuff! close video, etc
+    // TODO: handle video stream events
+    // TODO: gotta clean up previous stuff! close video, etc
 
-  var getUserMedia;
-  if (typeof navigator.webkitGetUserMedia === 'function') {
-    getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
-  } else if (typeof navigator.mozGetUserMedia === 'function') {
-    getUserMedia = navigator.mozGetUserMedia.bind(navigator);
-  } else {
-    getUserMedia = function () { alert("Your browser does not support camera input!"); };
-  }
-
-  getUserMedia({video: true}, function(_stream) {
-    buttons.enableButton();
-    this.setMessage('');
-    this.stream = _stream;
-    this.videoElem.src = window.URL.createObjectURL(_stream);
-    console.log("Video:", this.stream, this.videoElem, this.videoElem.src);
-
-    // this.stream.onended = function(e) { console.info("ended", e); };
-    // this.stream.onactive = function(e) { console.info("active", e); };
-    // this.stream.oninactive = function(e) { console.info("inactive", e); };
-    // TODO stream events
-  }.bind(this), function(e) {
-    if (e.name === 'DevicesNotFoundError') {
-      buttons.enableButton();
-      this.setMessage("You don't have a camera available!");
+    var getUserMedia;
+    if (typeof navigator.webkitGetUserMedia === 'function') {
+      getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+    } else if (typeof navigator.mozGetUserMedia === 'function') {
+      getUserMedia = navigator.mozGetUserMedia.bind(navigator);
     } else {
-      console.error("Couldn't start selfie video", e);
-      this.notifyFail();
+      getUserMedia = function () { alert("Your browser does not support camera input!"); };
     }
-  }.bind(this));
+
+    getUserMedia({video: true}, function(_stream) {
+      buttons.enableButton();
+      this.setMessage('');
+      this.stream = _stream;
+      this.videoElem.src = window.URL.createObjectURL(_stream);
+      console.log("Video:", this.stream, this.videoElem, this.videoElem.src);
+
+      // this.stream.onended = function(e) { console.info("ended", e); };
+      // this.stream.onactive = function(e) { console.info("active", e); };
+      // this.stream.oninactive = function(e) { console.info("inactive", e); };
+      // TODO stream events
+    }.bind(this), function(e) {
+      if (e.name === 'DevicesNotFoundError') {
+        buttons.enableButton();
+        this.setMessage("You don't have a camera available!");
+      } else {
+        console.error("Couldn't start selfie video", e);
+        this.notifyFail();
+      }
+    }.bind(this));
+  },
+
+  snapSelfie: function(dynamic, takeSelfieCallback, fileCallback) {
+    this.resizeCanvas(dynamic);
+    var ctx = this.canvasElem.getContext('2d');
+    this.selfieCountdown(takeSelfieCallback(this.videoElem, this.canvasElem, ctx, fileCallback));
+  },
+
+  // TODO: de-classify this
+
+  showCount: function (count, callback) {
+    if (count > 0) {
+      this.counterContainer.html('<h3 class="count">' + count + '</h3>');
+      setTimeout(function() { this.showCount(count - 1, callback); }.bind(this), 1000);
+    } else {
+      this.counterContainer.empty();
+      callback();
+    }
+  },
+  selfieCountdown: function(callback) {
+    this.showCount(3, callback);
+  }
 };
 
 // TODO
-  function notifyFail () {
-    $(videoSelector).remove();
-    $(canvasSelector).remove();
-    $(buttonSelector)
-      .prop('disabled', true)
-      .text('Something broke :(')
-      .addClass('btn-danger')
-      .children('span').remove();
-  }
-
-GitHubSelfieVideoPreview.prototype.snapSelfie = function(dynamic, takeSelfieCallback, fileCallback) {
-  this.resizeCanvas(dynamic);
-  var ctx = this.canvasElem.getContext('2d');
-  this.selfieCountdown(takeSelfieCallback(this.videoElem, this.canvasElem, ctx, fileCallback));
-};
-
-// TODO: de-classify this
-
-GitHubSelfieVideoPreview.prototype.showCount = function (count, callback) {
-  if (count > 0) {
-    this.counterContainer.html('<h3 class="count">' + count + '</h3>');
-    setTimeout(function() { this.showCount(count - 1, callback); }.bind(this), 1000);
-  } else {
-    this.counterContainer.empty();
-    callback();
-  }
-};
-GitHubSelfieVideoPreview.prototype.selfieCountdown = function(callback) {
-  this.showCount(3, callback);
-};
+function notifyFail() {
+  $(videoSelector).remove();
+  $(canvasSelector).remove();
+  $(buttonSelector)
+    .prop('disabled', true)
+    .text('Something broke :(')
+    .addClass('btn-danger')
+    .children('span').remove();
+}
 
 
 
@@ -248,17 +252,17 @@ function GitHubSelfies(config) {
     } else {
       $('.form-actions-protip').hide();
       buttons = new GitHubSelfieButtons();
-      buttons.insertBefore(candidate);
+      buttons.insert(candidate);
       setupEvents(buttons);
     }
   };
 
   function hideElements () {
-    $('.github-selfies').addClass(hiddenClass);
+    $('.github-selfies').addClass('hidden');
   }
 
   function showElements () {
-    $('.github-selfies').removeClass(hiddenClass);
+    $('.github-selfies').removeClass('hidden');
   }
 
   function setupEvents (buttons) {
@@ -370,6 +374,6 @@ function GitHubSelfies(config) {
       textarea.val()
         .replace(toReplace, '![selfie-' + number + '](' + link + ')'));
     textarea.focus();
-    textarea.setSelectionRange(textarea.textLength, textarea.textLength);
+    textarea.get(0).setSelectionRange(textarea.textLength, textarea.textLength);
   }
 }
