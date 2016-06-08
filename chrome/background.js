5c4cae3e2e64e35e41dd3a210592846f130f3171
Peter Dore <peter.dore@carezone.com>
2016-06-08 09:19:24 -0700

    Handle GitHub's updated Content Security Policy
    It looks like GitHub changed their CSP recently. This extension didn't work for me until I added this code.

--- chrome/background.js
+++ chrome/background.js
@@ -7,6 +7,8 @@ chrome.webRequest.onHeadersReceived.addListener(function(details) {
     if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
       var csp = details.responseHeaders[i].value;
       csp = csp.replace("media-src 'none'", "media-src 'self' blob:");
+      csp = csp.replace("connect-src 'self' uploads.github.com status.github.com api.github.com www.google-analytics.com github-cloud.s3.amazonaws.com wss://live.github.com",
+                        "connect-src 'self' uploads.github.com status.github.com api.github.com www.google-analytics.com github-cloud.s3.amazonaws.com wss://live.github.com api.imgur.com");
 
       details.responseHeaders[i].value = csp;
     }
