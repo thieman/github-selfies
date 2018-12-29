02026a9832273949eec88705e37ba1ae40784516
Travis Thieman <travis.thieman@gmail.com>
2018-12-29 14:13:26 -0500

    Style fixes to make things not look like a garbage fire

--- chrome/selfie-base.js
+++ chrome/selfie-base.js
@@ -369,6 +369,8 @@ GitHubSelfies.prototype = {
       //ctx.scale(-1, 1);
       ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, video.videoWidth, video.videoHeight);
       imgBinary = canvas.toDataURL('/image/jpeg', 1).split(',')[1];
+      // Hack, canvas is initially hidden before any pictures are taken
+      $(canvas).removeClass('hidden');
       ctx.restore();
       callback(imgBinary);
     };
@@ -440,6 +442,8 @@ GitHubSelfies.prototype = {
           frames.push([ctx.getImageData(0, 0, width, height).data, frameDelay]);
           this.buttons.videoPreview.setProgress(frameNum / totalFrames);
           if (frameNum >= totalFrames) {
+            // Hack, canvas is initially hidden before any pictures are taken
+            $(canvas).removeClass('hidden');
             cancelAnimationFrame(rafRequest);
             makeGif().then(() => this.buttons.videoPreview.setProgress(0));
           }
